8fa0b741111d62ddb17b77be658859a11d055a9f
Teemu Rytilahti <tpr@iki.fi>
2019-01-10 20:39:16 +0100

    Black on conftest

--- pyHS100/tests/conftest.py
+++ pyHS100/tests/conftest.py
@@ -6,17 +6,19 @@ from .newfakes import FakeTransportProtocol
 from os.path import basename
 from pyHS100 import SmartPlug, SmartBulb, SmartStrip, Discover
 
-SUPPORTED_DEVICES = glob.glob(os.path.dirname(os.path.abspath(__file__)) + "/fixtures/*.json")
+SUPPORTED_DEVICES = glob.glob(
+    os.path.dirname(os.path.abspath(__file__)) + "/fixtures/*.json"
+)
 # TODO add HS300 tests
 # SUPPORTED_DEVICES = [dev for dev in SUPPORTED_DEVICES if 'HS300' not in dev]
 
-BULBS = {'LB100', 'LB120', 'LB130'}
-VARIABLE_TEMP = {'LB120', 'LB130'}
-PLUGS = {'HS100', 'HS105', 'HS110', 'HS200', 'HS220', 'HS300'}
-STRIPS = {'HS300'}
-COLOR_BULBS = {'LB130'}
-DIMMABLE = {*BULBS, 'HS220'}
-EMETER = {'HS110', 'HS300', *BULBS}
+BULBS = {"LB100", "LB120", "LB130"}
+VARIABLE_TEMP = {"LB120", "LB130"}
+PLUGS = {"HS100", "HS105", "HS110", "HS200", "HS220", "HS300"}
+STRIPS = {"HS300"}
+COLOR_BULBS = {"LB130"}
+DIMMABLE = {*BULBS, "HS220"}
+EMETER = {"HS110", "HS300", *BULBS}
 
 ALL_DEVICES = BULBS.union(PLUGS)
 
@@ -32,21 +34,31 @@ def filter_model(filter):
     return filtered
 
 
-has_emeter = pytest.mark.parametrize('dev', filter_model(EMETER), indirect=True)
-no_emeter = pytest.mark.parametrize('dev', filter_model(ALL_DEVICES - EMETER), indirect=True)
+has_emeter = pytest.mark.parametrize("dev", filter_model(EMETER), indirect=True)
+no_emeter = pytest.mark.parametrize(
+    "dev", filter_model(ALL_DEVICES - EMETER), indirect=True
+)
 
-bulb = pytest.mark.parametrize('dev', filter_model(BULBS), indirect=True)
-plug = pytest.mark.parametrize('dev', filter_model(PLUGS), indirect=True)
-strip = pytest.mark.parametrize('dev', filter_model(STRIPS), indirect=True)
+bulb = pytest.mark.parametrize("dev", filter_model(BULBS), indirect=True)
+plug = pytest.mark.parametrize("dev", filter_model(PLUGS), indirect=True)
+strip = pytest.mark.parametrize("dev", filter_model(STRIPS), indirect=True)
 
-dimmable = pytest.mark.parametrize('dev', filter_model(DIMMABLE), indirect=True)
-non_dimmable = pytest.mark.parametrize('dev', filter_model(ALL_DEVICES - DIMMABLE), indirect=True)
+dimmable = pytest.mark.parametrize("dev", filter_model(DIMMABLE), indirect=True)
+non_dimmable = pytest.mark.parametrize(
+    "dev", filter_model(ALL_DEVICES - DIMMABLE), indirect=True
+)
 
-variable_temp = pytest.mark.parametrize('dev', filter_model(VARIABLE_TEMP), indirect=True)
-non_variable_temp = pytest.mark.parametrize('dev', filter_model(BULBS - VARIABLE_TEMP), indirect=True)
+variable_temp = pytest.mark.parametrize(
+    "dev", filter_model(VARIABLE_TEMP), indirect=True
+)
+non_variable_temp = pytest.mark.parametrize(
+    "dev", filter_model(BULBS - VARIABLE_TEMP), indirect=True
+)
 
-color_bulb = pytest.mark.parametrize('dev', filter_model(COLOR_BULBS), indirect=True)
-non_color_bulb = pytest.mark.parametrize('dev', filter_model(BULBS - COLOR_BULBS), indirect=True)
+color_bulb = pytest.mark.parametrize("dev", filter_model(COLOR_BULBS), indirect=True)
+non_color_bulb = pytest.mark.parametrize(
+    "dev", filter_model(BULBS - COLOR_BULBS), indirect=True
+)
 
 
 # Parametrize tests to run with device both on and off
@@ -75,22 +87,20 @@ def dev(request):
     with open(file) as f:
         sysinfo = json.load(f)
         model = basename(file)
-        if 'LB' in model:
+        if "LB" in model:
             p = SmartBulb("123.123.123.123", protocol=FakeTransportProtocol(sysinfo))
-        elif 'HS300' in model:
+        elif "HS300" in model:
             p = SmartStrip("123.123.123.123", protocol=FakeTransportProtocol(sysinfo))
-        elif 'HS' in model:
+        elif "HS" in model:
             p = SmartPlug("123.123.123.123", protocol=FakeTransportProtocol(sysinfo))
         else:
             raise Exception("No tests for %s" % model)
         yield p
 
 
-
 def pytest_addoption(parser):
-    parser.addoption(
-        "--ip", action="store", default=None, help="run against device"
-    )
+    parser.addoption("--ip", action="store", default=None, help="run against device")
+
 
 """
 def pytest_generate_tests(metafunc):
@@ -106,9 +116,8 @@ def pytest_generate_tests(metafunc):
 """
 
 
-
-#@pytest.yield_fixture(scope='session')
-#def pg_server(pg_tag):
+# @pytest.yield_fixture(scope='session')
+# def pg_server(pg_tag):
 
 
 def pytest_collection_modifyitems(config, items):
